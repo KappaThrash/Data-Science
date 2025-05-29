003262e8a069c24f19b7ffc392b8c630f69caa9d
KappaThrash <danielkuhin@gmail.com>
2025-05-29 13:24:53 -0300

    Site

---
+++ bv.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+import streamlit as st
+import matplotlib as mtl
+
+st.set_page_config(layout='wide')
+
+df_reviews = pd.read_csv('customerreviews.csv')
+df_top = pd.read_csv('Top-100-Trending Books.csv')
+
+topbookmin = df_top['book price'].min()
+topbookmax = df_top['book price'].max()
+
+max_price = st.sidebar.slider(label='Slider de preço dos livros', min_value=topbookmin, max_value=topbookmax, value=topbookmax)
+df_bookprice_filtered = df_top[df_top['book price'] <= max_price]
+df_bookprice_filtered
